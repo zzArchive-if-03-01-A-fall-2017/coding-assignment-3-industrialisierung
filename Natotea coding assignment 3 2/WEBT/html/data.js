9296c07a1d846f0ad53b6594c1b713d692265ff3
Tadic Sandro <if160149@WS8A17.local>
2018-04-30 09:45:40 +0200

    my commit

--- Natotea coding assignment 3 2/WEBT/html/data.js
+++ Natotea coding assignment 3 2/WEBT/html/data.js
@@ -1,9 +1,23 @@
+window.onload = function()
+{
+  let storeButton = document.getElementById('login');
+  storeButton.onclick = function()
+  {
+    let psw = document.getElementById('psw');
+    let uname = document.getElementById('uname');
 
-function setStore() {
-  var uname = document.getElementById('uname');
-  var psw = document.getElementById('psw');
+    localStorage.setItem(psw.id,psw.value);
+    localStorage.setItem(uname.id,uname.value);
+  }
+  let registerButton = document.getElementById('register')
+  registerButton.onclick = function()
+  {
+    let pswRegister = document.getElementById('pswRegister');
+    let pswRegister2 = document.getElementById('pswRegister2');
+    let unameRegister = document.getElementById('unameRegister');
 
-  localStorage.setItem("UserName",uname);
-  localStorage.setItem("Password",psw);
-
+    localStorage.setItem(pswRegister.id,pswRegister.value);
+    localStorage.setItem(pswRegister2.id,pswRegister2.value);
+    localStorage.setItem(unameRegister.id,unameRegister.value);
+  }
 }
