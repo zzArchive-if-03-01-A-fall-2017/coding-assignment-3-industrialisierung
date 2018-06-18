21d8fcec85f76b051838398c1c45e9ed38706999
Tadic Sandro <if160149@WS8A17.htl-leonding.ac.at>
2018-06-18 09:46:17 +0200

    commit

--- Natotea coding assignment 3 2/WEBT/html/data.js
+++ Natotea coding assignment 3 2/WEBT/html/data.js
@@ -1,32 +1,28 @@
 window.onload = function()
 {
-
-  let xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-  xhr.onreadystatechange = function(){
-    let storeButton = document.getElementById('login');
-    storeButton.onclick = function()
-    {
-      let psw = document.getElementById('psw');
-      let uname = document.getElementById('uname');
-
-      localStorage.setItem(psw.id,psw.value);
-      localStorage.setItem(uname.id,uname.value);
+  let registerButton = document.querySelector("#submitRegistration");
+  registerButton.onclick = function() {
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let url = "http://leotux.htl-leonding.ac.at:3000/users";
+    xhr.open('POST', url);
+    xhr.setRequestHeader("Content-type","application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status === 200) {
+        alert("Registration succesful");
+      }
     }
-    let registerButton = document.getElementById('register')
-    registerButton.onclick = function()
-    {
-      let pswRegister = document.getElementById('pswRegister');
-      let pswRegister2 = document.getElementById('pswRegister2');
-      let unameRegister = document.getElementById('unameRegister');
-
-      localStorage.setItem(pswRegister.id,pswRegister.value);
-      localStorage.setItem(pswRegister2.id,pswRegister2.value);
-      localStorage.setItem(unameRegister.id,unameRegister.value);
-    }
+    let jsonString = JSON.stringify(getRequestBody());
+    xhr.send(jsonString);
   }
-  xhr.open('POST','server.at/Dokumente/UpToDate/Industrialisierung/Natotea coding assignment 3 2/WEBT/JSON/Storage.json');
-  //xhr.setRequestHeader("Content-type","application/json");
-  xhr.setRequestHeader("Usernames","application/json");
-  xhr.send(JSON.stringify(getRequestBody());
+}
+
+function getRequestBody() {
+  let pswRegister = document.getElementById('pswRegister').value;
+  let pswRegister2 = document.getElementById('pswRegister2').value;
+  let unameRegister = document.getElementById('unameRegister').value;
+  return {
+    username: unameRegister,
+    password: pswRegister
+  };
 }
