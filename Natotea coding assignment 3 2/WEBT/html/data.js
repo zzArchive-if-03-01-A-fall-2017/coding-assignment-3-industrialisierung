43c936553aaa23a705ddec22c0b76321107138d3
Tadic Sandro <if160149@WS8A17.htl-leonding.ac.at>
2018-06-25 09:44:43 +0200

    commit

--- Natotea coding assignment 3 2/WEBT/html/data.js
+++ Natotea coding assignment 3 2/WEBT/html/data.js
@@ -1,19 +1,48 @@
 window.onload = function()
 {
+  let url = "http://leotux.htl-leonding.ac.at:3000/users";
   let registerButton = document.querySelector("#submitRegistration");
   registerButton.onclick = function() {
+    let pswRegister2ForCheck = document.getElementById('pswRegister2').value;
+    let pswRegisterForCheck = document.getElementById('pswRegister').value;
+
+    if(pswRegister2ForCheck != pswRegisterForCheck){
+      alert("Diffrent passwords!");
+    }
+    else {
+      let xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.open('POST', url);
+      xhr.setRequestHeader("Content-type","application/json");
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          alert("Registration succesful");
+        }
+      }
+      let jsonString = JSON.stringify(getRequestBody());
+      xhr.send(jsonString);
+    }
+  }
+  let loginButton = document.querySelector("#getLogin");
+  loginButton.onclick = function() {
+    alert("Check if it goes in");
+    let loginName = document.getElementById('userName').value;
+    let loginPsw = document.getElementById('psw').value;
     let xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    let url = "http://leotux.htl-leonding.ac.at:3000/users";
-    xhr.open('POST', url);
+    xhr.open('GET',url);
     xhr.setRequestHeader("Content-type","application/json");
+    var data = xhr.responseText;
+    var uploadResult = data['message']
+    console.log('responseText:',uploadResult);
+
     xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        alert("Registration succesful");
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        alert("Login succesful");
       }
     }
-    let jsonString = JSON.stringify(getRequestBody());
-    xhr.send(jsonString);
+    xhr.send();
   }
 }
 
